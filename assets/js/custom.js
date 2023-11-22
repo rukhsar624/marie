6ee38afdfe6cb56da9fc3dc92c25bf5d0db6a41f
rukhsar624 <rukhsaranwar56@gmail.com>
2023-11-22 16:45:28 +0500

    add animations

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,6 @@
+new WOW({
+    offset: 200
+  }).init();
 var animateHTML = function() {
     var elems;
     var windowHeight;
@@ -28,36 +31,20 @@ var animateHTML = function() {
    };
    animateHTML().init();
 //    loader js open
-
-  // Function to show the loader
   function showLoader() {
     document.getElementById('loader').style.display = 'flex';
   }
-
-  // Function to hide the loader
   function hideLoader() {
     document.getElementById('loader').style.display = 'none';
   }
-
-  // Add an event listener to the window to show the loader when navigating
   window.addEventListener('beforeunload', showLoader);
-
-  // If your navigation is done using AJAX or a single-page application framework,
-  // you may need to use appropriate events for navigation start and end.
-  // For example, if using jQuery for AJAX:
-  // $(document).ajaxStart(showLoader);
-  // $(document).ajaxStop(hideLoader);
-
-  // Example: simulate a delay to see the loader
   document.addEventListener('DOMContentLoaded', function() {
-    // Simulate a delay (you can remove this in a real scenario)
     showLoader();
-
-    // Simulate a longer delay for demonstration purposes
     setTimeout(function() {
       hideLoader();
-    }, 2000); // Adjust the timeout duration here
+    }, 2000);
   });
 
 
 // loader js closed
+
