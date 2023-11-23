4c8c9d56730cc73cf14d0399d98c19345e4d81b7
rukhsar624 <rukhsaranwar56@gmail.com>
2023-11-23 17:06:44 +0500

    add more animations

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -31,20 +31,39 @@ var animateHTML = function() {
    };
    animateHTML().init();
 //    loader js open
-  function showLoader() {
-    document.getElementById('loader').style.display = 'flex';
-  }
-  function hideLoader() {
-    document.getElementById('loader').style.display = 'none';
-  }
-  window.addEventListener('beforeunload', showLoader);
-  document.addEventListener('DOMContentLoaded', function() {
-    showLoader();
-    setTimeout(function() {
-      hideLoader();
-    }, 2000);
-  });
-
-
+  // function showLoader() {
+  //   document.getElementById('loader').style.display = 'flex';
+  // }
+  // function hideLoader() {
+  //   document.getElementById('loader').style.display = 'none';
+  // }
+  // window.addEventListener('beforeunload', showLoader);
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   showLoader();
+  //   setTimeout(function() {
+  //     hideLoader();
+  //   }, 2000);
+  // });
 // loader js closed
+// text animation
+const t1 = gsap.timeline();
+        const staggeredFlipInText = new SplitText(".staggered-flip-in", {
+            type: "chars"
+        });
+        const staggeredFlipInChars = staggeredFlipInText.chars;
+        t1.set(staggeredFlipInChars, {
+            transformOrigin: "center center 40px",
+            backfaceVisibility: "hidden"
+        });
+        t1.from(staggeredFlipInChars, {
+            opacity: 1,
+            rotateY: -120,
+            duration: 1,
+            stagger: 0.04,
+            ease: "cubic-bezier(0.425, 0.005, 0.0, 1.0)"
+        });
+// text animation
+// primary num
+
+// primary num
 
